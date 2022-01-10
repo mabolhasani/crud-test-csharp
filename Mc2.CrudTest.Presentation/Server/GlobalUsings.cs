02fbae39e5acec44ad427325d119a3837775dbf7
Mohammad <mohammad.abolhasany@gmail.com>
2022-01-10 21:33:20 +0330

    added global usings file

---
+++ Mc2.CrudTest.Presentation/Server/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using Microsoft.EntityFrameworkCore;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Linq.Expressions;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.Reflection;
