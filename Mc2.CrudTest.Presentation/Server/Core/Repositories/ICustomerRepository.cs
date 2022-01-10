e7ecbbada707f022e3f418047ad7029b65f678b4
Mohammad <mohammad.abolhasany@gmail.com>
2022-01-10 21:49:23 +0330

    added customer repository and its interface

---
+++ Mc2.CrudTest.Presentation/Server/Core/Repositories/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mc2.CrudTest.Presentation.Server.Core.Domain;
+using Mc2.CrudTest.Presentation.Server.Core.Repositories.Base;
+
+namespace Mc2.CrudTest.Presentation.Server.Core.Repositories;
+
+public interface ICustomerRepository : IRepository<Customer>
+{
+}
+
